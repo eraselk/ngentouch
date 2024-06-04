70d453c78bb04bbc153f0ec932f0c9989aa080c7
eraselk-cli <eraselk@android-dev>
2024-06-04 16:24:13 +0800

    Sync NgenTouch Source

--- NgenTouch/build.sh
+++ NgenTouch/build.sh
@@ -1,11 +1,11 @@
 export PATH="/data/data/com.termux/files/usr/bin:${PATH}"
-VER="$(cat ./module.prop | grep 'version=' | cut -f 2 -d '=')"
-build_date=$(date +"%Y-%m-%d")
+VER="$(cat ./module.prop | grep 'version=' | cut -f 2 -d '=' | awk '{print $1}')"
+build_date=$(date +"%y%m%d")
 stamp=$(date +"%H%M%S")
 
 ######################################
 module_name="NgenTouch"
-zip_name="${module_name}_${VER}_${build_date}_${stamp}.zip"
+zip_name="${module_name}-${VER}-${build_date}${stamp}.zip"
 # 0 = Disable 1 = Enable
 remove_bak="1"
 #####################################
@@ -21,3 +21,5 @@ zip -r9 "$zip_name" * -x build.sh*
 mv -f ./"$zip_name" ..
 
 mv -f $dir/ntm $dir/ntm.sh
+
+sed -i "s/version=$VER/version=$VER ($build_date)/" ./module.prop
--- NgenTouch/install.sh
+++ NgenTouch/install.sh
@@ -8,15 +8,21 @@ SKIPMOUNT=false
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
+DEBUGGING=0
 
 mod_print() {
+
+if [[ "$ARCH" != "arm64" ]]; then
+abort "- Not supported arch."
+fi
+
 # Load function
 cmd_pkg() {
     a="cmd package"
     if cmd package "$@" >/dev/null 2>&1; then
-        echo "[INFO] $a $@ : Success" 
+        ui_print "[INFO] $a $@ : Success" 
     else
-        echo "[ERROR] $a $@ : Failed"
+        ui_print "[ERROR] $a $@ : Failed"
     fi
 }
 
@@ -35,8 +41,7 @@ ui_print "_____________________________________________"
 ui_print "   Feel The Responsiveness and Smoothess !  "
 ui_print ""
 sleep 1
-ui_print "[•] Patching package com.android.systemui"
-{
+if ! [[ "$DEBUGGING" == "1" ]]; then
 cmd_pkg compile -m verify -f com.android.systemui
 cmd_pkg compile -m assume-verified -f com.android.systemui --compile-filter=assume-verified -c --reset
 sleep 5
@@ -52,27 +57,61 @@ cmd_pkg compile -r shared --secondary-dex -f com.android.systemui
 cmd_pkg compile -r shared --secondary-dex com.android.systemui
 cmd_pkg reconcile-secondary-dex-files com.android.systemui
 ui_print ""
-}
-if [[ $? -eq 0 ]]; then
-	ui_print "[√] Done"
-else
-	ui_print "[!] Failed"
 fi
 ui_print ""
 sleep 1
+if cmd package -l | grep 'com.termux' > /dev/null 2>&1; then
+ui_print "- Termux app detected."
+tmux="/data/data/com.termux/files/usr/etc"
+tmux_home="/data/data/com.termux/files/home"
+if ! [[ $(grep -c '# Modified by NgenTouch' $tmux/bash.bashrc) -gt "0" ]]; then
+ui_print "- Patching termux.."
+touch $tmux_home/.ngentouch
+[[ ! -w "$tmux/bash.bashrc" ]] && chmod +w $tmux/bash.bashrc
+echo "" >> $tmux/bash.bashrc
+(
+cat <<"EOF"
+if [[ -f "/data/data/com.termux/files/home/.ngentouch" ]]; then
+echo ""
+echo "EN"
+echo "Please igrone the warning."
+echo ""
+echo "ID"
+echo "Hiraukan peringatan (WARNING) yang muncul."
+if ! pkg list-installed | grep 'curl' 2>&1 > /dev/null; then
+pkg update -y && pkg upgrade -y && pkg install -y curl
+fi
+if ! pkg list-installed | grep 'wget' 2>&1 > /dev/null; then
+pkg update -y && pkg upgrade -y && pkg install -y wget
+fi
+if ! pkg list-installed | grep 'openssl' 2>&1 > /dev/null; then
+pkg update -y && pkg upgrade -y && pkg install -y openssl
+fi
+if ! pkg list-installed | grep 'openssh' 2>&1 > /dev/null; then
+pkg update -y && pkg upgrade -y && pkg install -y openssh
+fi
+rm -f /data/data/com.termux/files/home/.ngentouch
+fi
+# Modified by NgenTouch module
+EOF
+) >> $tmux/bash.bashrc
+ui_print "- After the module installed don't reboot your phone
+  but open the termux app and wait for installing needed packages."
+fi
+else
+abort "- Please install termux app from F-Droid, then reinstall this module."
+fi
 }
 
 extract_files() {
 unzip -o "$ZIPFILE" 'service.sh' -d $MODPATH >&2
 unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 
-if [[ "$ARCH" != *arm* ]]; then
-ui_print "- Can't install this module for your ARCH"
-ui_print "- Your ARCH is: $ARCH"
-exit 1
-else
 unzip -o "$ZIPFILE" 'debugger' -d $MODPATH/system/bin >&2
-fi
+unzip -o "$ZIPFILE" 'shared_lib.tar.gz' -d $MODPATH >&2
+
+tar -xf $MODPATH/shared_lib.tar.gz -C $MODPATH/ >&2
+rm -f $MODPATH/shared_lib.tar.gz
 }
 
 perm() {
