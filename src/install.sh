70fcc14c893aa0824ed14eeea920ab15a898ac1e
whoami <you@example.com>
2024-07-15 18:28:01 +0800

    install.sh: improve

--- src/install.sh
+++ src/install.sh
@@ -8,6 +8,7 @@ SKIPMOUNT=false
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
+dbg=1
 
 mod_print() {
 
@@ -30,6 +31,7 @@ mod_print() {
     ui_print "   Feel The Responsiveness and Smoothess !  "
     ui_print ""
     sleep 1
+    if [ $dbg -ne 1 ]; then
     cmd_pkg compile -m verify -f com.android.systemui
     cmd_pkg compile -m assume-verified -f com.android.systemui --compile-filter=assume-verified -c --reset
     cmd_pkg force-dex-opt com.android.systemui
@@ -43,9 +45,73 @@ mod_print() {
     cmd_pkg compile -r shared --secondary-dex -f com.android.systemui
     cmd_pkg compile -r shared --secondary-dex com.android.systemui
     cmd_pkg reconcile-secondary-dex-files com.android.systemui
+    fi
     ui_print ""
     ui_print ""
     sleep 1
+    
+    ui_print " Testing cmd... (1/2)"
+    if settings put global test 1; then
+        sleep 0.4
+        ui_print " Normal method: OK"
+        test_cmd1=1
+    else
+        ui_print " Normal method: ERROR"
+        ui_print ""
+        sleep 0.2
+        ui_print " Trying another method..."
+        sleep 0.2
+        if su -lp 2000 -c "settings put global test 1"; then
+            ui_print " Another method: OK"
+            another1=1
+        else
+            ui_print " Another method: ERROR"
+            another1=0
+        fi
+    fi
+    
+    ui_print ""
+    ui_print " Testing cmd... (2/2)"
+    if settings get global test >/dev/null 2>&1; then
+        ui_print " Normal method: OK"
+        test_cmd2=1
+    else
+        ui_print " Normal method: ERROR"
+        ui_print ""
+        sleep 0.2
+        ui_print " Trying another method..."
+        sleep 0.2
+        if su -lp 2000 -c "settings get global test >/dev/null 2>&1"; then
+            ui_print " Another method: OK"
+            another2=1
+        else
+            ui_print " Another method: ERROR"
+            another2=0
+        fi
+    fi
+    
+    ui_print ""
+    if [ $test_cmd1 -eq 1 ] && [ $test_cmd2 -eq 1 ]; then
+        ui_print " Result: use normal method"
+        normal_method=1
+    fi
+    
+    if [ $another1 -eq 1 ] && [ $another2 -eq 1 ]; then
+        ui_print " Result: use another method"
+        another_method=1
+    fi
+    
+    if [ $another1 -eq 0 ] && [ $another2 -eq 0 ]; then
+        ui_print " Result: ERROR"
+    fi
+    
+    if [ $normal_method -eq 1 ]; then
+        settings delete global test >/dev/null 2>&1
+    elif [ $another_method -eq 1]; then
+        su -lp 2000 -c "settings delete global test"
+    fi
+    
+    ui_print ""  
 }
 
 extract_files() {
@@ -57,7 +123,16 @@ perm() {
     set_perm_recursive $MODPATH 0 0 0777 0777
 }
 
+apply_method() {
+if [ $normal_method -eq 1 ]; then
+    sed -i "s/normal_method=0/normal_method=1/g" $MODPATH/system/bin/ntm
+elif [ $another_method -eq 1 ]; then
+    sed -i "s/another_method=0/another_method=1/g" $MODPATH/system/bin/ntm
+fi
+}
+
 set -x
 mod_print
 extract_files
 perm
+apply_method
