e88ea7c08790b728038c8383a58ff0234c13808b
eraselk <eraselk@proton.me>
2024-08-05 12:47:58 +0800

    update build script

--- install.sh
+++ install.sh
@@ -2,35 +2,44 @@
 # Install from source
 
 su -c "
-ABI="$(getprop ro.product.cpu.abi)"
+ABI=\$(getprop ro.product.cpu.abi)
 
-case "$ABI" in
-    "arm64-v8a") BIT="64" ;;
-    "armeabi-v7a") BIT="32" ;;
+case \$ABI in
+    arm64-v8a) BIT=64 ;;
+    armeabi-v7a) BIT=32 ;;
 esac
 
-KSU="/data/adb/ksu/ksud"
-AP="/data/adb/ap/apd"
-MAGISK="/data/adb/magisk/magisk$BIT"
+KSU=/data/adb/ksu/ksud
+AP=/data/adb/ap/apd
+MAGISK=/data/adb/magisk/magisk\$BIT
 
-if [ -f "$KSU" ]; then
-    CMD="$KSU"
-    ARG="module install"
-elif [ -f "$AP" ]; then
-    CMD="$AP"
-    ARG="module install"
-elif [ -f "$MAGISK" ]; then
-    CMD="$MAGISK"
-    ARG="--install-module"
+CMD=
+ARG=
+
+if [ -f \$KSU ]; then
+    CMD=\$KSU
+    ARG='module install'
+elif [ -f \$AP ]; then
+    CMD=\$AP
+    ARG='module install'
+elif [ -f \$MAGISK ]; then
+    CMD=\$MAGISK
+    ARG='--install-module'
 fi
 
-# <CMD> <ARG> <ZIP>
-find . -maxdepth 1 -type f -name '*.zip' | while read -r arr
-    for i in $arr; do
-        if [ "$i" = '*NgenTouch*' ]; then
-            ZN="$i"
-            break
-        fi
-    done
-$CMD $ARG $ZN
+if [ -z \$CMD ]; then
+    echo 'Error: No supported installation tool found.'
+    exit 1
+fi
+
+MODULE_NAME='NgenTouch'
+ZIP_FILE=\$(find . -maxdepth 1 -type f -name '*.zip' -exec echo {} \;)
+
+if [ -z \$ZIP_FILE ]; then
+    echo 'Error: Module ZIP file not found.'
+    exit 1
+fi
+
+echo 'Installing module: '\$MODULE_NAME
+\$CMD \$ARG \$ZIP_FILE
 "
