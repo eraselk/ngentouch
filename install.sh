c390c2f115d3029e7f215730ba7bf72128027b32
eraselk <eraselk@proton.me>
2024-08-06 18:23:47 +0800

    installer: add support for latest magisk

--- install.sh
+++ install.sh
@@ -1,35 +1,25 @@
 #!/system/bin/sh
 # Install from source
 
+if ! command -v su &>/dev/null; then
+echo "Are you root user??" >&2
+exit 1
+fi
+
 su -c "
-ABI=\$(getprop ro.product.cpu.abi)
-
-case \$ABI in
-    arm64-v8a) BIT=64 ;;
-    armeabi-v7a) BIT=32 ;;
-esac
-
-KSU=/data/adb/ksu/ksud
-AP=/data/adb/ap/apd
-MAGISK=/data/adb/magisk/magisk\$BIT
 
 CMD=
 ARG=
 
-if [ -f \$KSU ]; then
-    CMD=\$KSU
+if command -v ksud &>/dev/null; then
+    CMD='ksud'
     ARG='module install'
-elif [ -f \$AP ]; then
-    CMD=\$AP
+elif command -v apd &>/dev/null; then
+    CMD='apd'
     ARG='module install'
-elif [ -f \$MAGISK ]; then
-    CMD=\$MAGISK
+elif command -v magisk &>/dev/null; then
+    CMD='magisk'
     ARG='--install-module'
-fi
-
-if [ -z \$CMD ]; then
-    echo 'Error: No supported installation tool found.'
-    exit 1
 fi
 
 MODULE_NAME='NgenTouch'
