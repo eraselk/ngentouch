5c11876910611fa6dd8465e15f4646300d6ececc
eraselk <eraselk@proton.me>
2024-08-27 13:01:09 +0800

    Revert "Add touchpanel tweak special for mediatek device"
    This reverts commit 5f9cae02cd8fa0f48c6b0ff66447324e03d9305c.

--- src/system/bin/ntm.sh
+++ src/system/bin/ntm.sh
@@ -6,7 +6,6 @@
 
 # Automatically sets by the installer
 BB=
-mtk=false
 
 prerr() {
     echo -e "$1\a" >&2
@@ -80,13 +79,6 @@ run() {
         $BB chrt -f -p 99 $tid
     done
 
-    # Special for mediatek
-    if $mtk; then
-        pid=$(ps -Ao pid,name | awk '/irq/ && /tp/ {print $1}')
-        $BB renice -n -20 -p $pid
-        $BB chrt -f -p 99 $pid
-    fi
-    
     # always return success
     true
 }
